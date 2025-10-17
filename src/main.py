2688b4f9a498fef2b7e0d5e9190b6c0d2e73567f
Yubo Wang <yubowang2007@gmail.com>
2025-10-17 18:10:34 +0100

    Add basic templates file structure

--- src/main.py
+++ src/main.py
@@ -2,7 +2,17 @@ import random
 import os
 import sqlite3
 from datetime import datetime
-from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for,
+    flash,
+    jsonify,
+    send_from_directory,
+)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,9 +20,11 @@ load_dotenv()
 app = Flask(__name__)
 API_KEY = os.getenv("PPLX_API_KEY")
 
-@app.route("/home", methods=['GET', 'POST'])
-def home():
-    return render_template("home.html")
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True)
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
